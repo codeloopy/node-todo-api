8daac63a1d3a8a9de9d2ec4f4352ba648c6d5474
Pedro <codeloopy@gmail.com>
2019-01-07 16:31:16 -0500

    Init with some playground files

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,43 @@
+const MongoClient = require("mongodb").MongoClient;
+
+MongoClient.connect(
+  "mongodb://localhost:27017/TodoApp",
+  (error, client) => {
+    if (error) {
+      return console.log("Unable to connect to MongoDB");
+    }
+
+    console.log("Connected to MongoDB");
+    const db = client.db("TodoApp");
+
+    // db.collection("Todos").insertOne(
+    //   {
+    //     text: "My first todo",
+    //     completed: false
+    //   },
+    //   (error, result) => {
+    //     if (error) {
+    //       return console.log("Unable to insert todo");
+    //     }
+    //     console.log(JSON.stringify(result.ops, undefined, 2));
+    //   }
+    // );
+
+    db.collection("User").insertOne(
+      {
+        name: "Pedro",
+        age: 44,
+        location: "Florida"
+      },
+      (error, result) => {
+        if (error) {
+          return console.log("Unable to save the user", err);
+        }
+
+        console.log(JSON.stringify(result.ops, undefined, 2));
+      }
+    );
+
+    client.close();
+  }
+);
---
+++ playground/mongodb-find.js
@@ -0,0 +1,60 @@
+// const MongoClient = require("mongodb").MongoClient;
+const { MongoClient, ObjectId } = require("mongodb"); //destructuring mongodb
+
+MongoClient.connect(
+  "mongodb://localhost:27017/TodoApp",
+  (error, client) => {
+    if (error) {
+      return console.log("Unable to connect to MongoDB");
+    }
+
+    console.log("Connected to MongoDB");
+    const db = client.db("TodoApp");
+
+    console.log(
+      // db
+      //   .collection("Todos")
+      //   // .find() //this queries everything
+      //   .find({
+      //     _id: new ObjectId("5c32b969e7673eaa4aa15d36")
+      //   })
+      //   // .find({ completed: false }) //passing an argument fetches the completed with false
+      //   .toArray()
+      //   .then(
+      //     docs => {
+      //       console.log(JSON.stringify(docs, undefined, 2));
+      //     },
+      //     err => {
+      //       console.log("Unable to retrieve anything", err);
+      //     }
+      //   )
+      // db
+      //   .collection("Todos")
+      //   .find()
+      //   .count()
+      //   .then(
+      //     count => {
+      //       console.log(`Todos count: ${count}`);
+      //     },
+      //     err => {
+      //       console.log("Unable to retrieve anything", err);
+      //     }
+      //   )
+      db
+        .collection("User")
+        .find({ name: "Pedro" })
+        .toArray()
+        // .count()
+        .then(
+          docs => {
+            console.log(JSON.stringify(docs, undefined, 2));
+          },
+          err => {
+            console.log("Error!");
+          }
+        )
+    );
+
+    client.close();
+  }
+);
