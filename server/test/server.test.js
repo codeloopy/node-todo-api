c1f8bdbe94f5f0bc5393543269b653364bb1402f
Pedro <codeloopy@gmail.com>
2019-01-09 18:10:12 -0500

    Modified server.test.js file to accmmodate for some seed data

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -4,9 +4,15 @@ const request = require("supertest");
 const { app } = require("./../server");
 const { Todo } = require("./../models/todo");
 
+const todos = [{ text: "First test todo" }, { text: "Second test todo" }];
+
 beforeEach(done => {
-  // Todo.remove({}).then(() => done());
-  Todo.deleteMany({}).then(() => done());
+  // Todo.remove({}).then(() => done());  //remove is depricated
+  Todo.deleteMany({})
+    .then(() => {
+      return Todo.insertMany(todos);
+    })
+    .then(() => done());
 });
 
 describe("POST /todos", () => {
@@ -25,7 +31,7 @@ describe("POST /todos", () => {
           return done(err);
         }
 
-        Todo.find()
+        Todo.find({ text })
           .then(todos => {
             expect(todos.length).toBe(1);
             expect(todos[0].text).toBe(text);
@@ -47,7 +53,7 @@ describe("POST /todos", () => {
 
         Todo.find()
           .then(todos => {
-            expect(todos.length).toBe(0);
+            expect(todos.length).toBe(2);
             done();
           })
           .catch(e => done(e));
