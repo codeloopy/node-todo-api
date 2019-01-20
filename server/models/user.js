26c6aa153bdc30bf0a038c0fab867d1d3f8fa761
Pedro <codeloopy@gmail.com>
2019-01-20 00:39:30 -0500

    Add POST users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -7,7 +7,7 @@ const bcrypt = require("bcryptjs");
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: false,
     trim: true,
     minlength: 5
   },
@@ -78,6 +78,26 @@ UserSchema.statics.findByToken = function(token) {
     _id: decoded._id,
     "tokens.token": token,
     "tokens.access": "auth"
+  });
+};
+
+UserSchema.statics.findByCredentials = function(email, password) {
+  const User = this;
+
+  return User.findOne({ email }).then(user => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
   });
 };
 
--- server/server.js
+++ server/server.js
@@ -164,6 +164,24 @@ app.get("/users/me", authenticate, (req, res) => {
   res.send(req.user);
 });
 
+// POST /users/login {email, password}
+// setup route, pick email and password off from the req. and to verify
+// use res.send to send back the body data
+
+app.post("/users/login", (req, res) => {
+  const body = _.pick(req.body, ["email", "password"]);
+
+  User.findByCredentials(body.email, body.password)
+    .then(user => {
+      return user.generateAuthToken().then(token => {
+        res.header("x-auth", token).send(user);
+      });
+    })
+    .catch(e => {
+      res.status(400).send();
+    });
+});
+
 /* ***************** */
 /*                   */
 /*    SERVER RUN     */
